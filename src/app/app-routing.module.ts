a91d394f811d79806b073e25c7d3eb29a334d4b3
Yannick Holzenkamp <yannick@holzenkamp.me>
2020-10-01 16:54:16 +0200

    add basic ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Step1authorizeComponent } from './steps/step1authorize/step1authorize.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'step-1', component: Step1authorizeComponent },
+  { path: '',   redirectTo: '/step-1', pathMatch: 'full' },
+  { path: '**', redirectTo: '/step-1' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
